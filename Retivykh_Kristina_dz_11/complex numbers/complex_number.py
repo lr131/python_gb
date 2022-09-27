7874d533c144f49a1813372dd5203931e212e6a3
lr131 <lr131@localhost.localdomain>
2022-09-28 01:44:27 +0800

    complex

--- Retivykh_Kristina_dz_11/complex numbers/complex_number.py
+++ Retivykh_Kristina_dz_11/complex numbers/complex_number.py
@@ -1,5 +1,36 @@
 class ComplexNumber:
-    def __int__(self, real, imaginary):
-        # Проверить, что это числа
-        self.real = real
-        self.imaginary = imaginary
+    
+    def __init__(self, real, imaginary):
+        if isinstance(real, int | float):
+            self.real = real
+        elif isinstance(real, str) and (real.isdigit):
+            self.real = float(real)
+        else:
+            raise TypeError
+        if isinstance(imaginary, int | float):
+            self.imaginary = imaginary
+        elif isinstance(imaginary, str) and (imaginary.isdigit):
+            self.imaginary = float(imaginary)
+        else:
+            raise TypeError
+        
+    def __add__(self, obj):
+        return f'z = {self.real + obj.real} + {self.imaginary + obj.imaginary}*i'
+
+    def __mul__(self, obj):
+        return f'z = {self.real * obj.real - (self.imaginary * obj.imaginary)} + {self.imaginary * obj.real}*i'
+
+    def __str__(self):
+        return f'z = {self.real} + {self.imaginary}*i'
+    
+    
+if __name__ == '__main__':
+    z1 = ComplexNumber(1, 3)
+    z2 = ComplexNumber(7, 2)
+    
+    print(z1)
+    print(z1 + z2)
+    print(z1 * z2)
+    
+    z3 = ComplexNumber('5.2', 4)
+    print(z3)
