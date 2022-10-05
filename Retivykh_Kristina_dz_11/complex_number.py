a453c4d66742b9eed479ff1eadb78642402388b6
lr131 <lr131@localhost.localdomain>
2022-10-05 23:53:33 +0800

    fixed задача про комплексные, добавила вычитание и поправила вид

--- Retivykh_Kristina_dz_11/complex_number.py
+++ Retivykh_Kristina_dz_11/complex_number.py
@@ -16,21 +16,30 @@ class ComplexNumber:
         
     def __add__(self, obj):
         return ComplexNumber(self.real + obj.real, self.imaginary + obj.imaginary)
+    
+    def __sub__(self, obj):
+        return ComplexNumber(self.real - obj.real, self.imaginary - obj.imaginary)
+        
 
     def __mul__(self, obj):
         return ComplexNumber(self.real * obj.real - (self.imaginary * obj.imaginary), self.imaginary * obj.real)
 
+    def __eq__(self, other):
+        return (self.real == other.real) and (self.imaginary == other.imaginary)
+    
     def __str__(self):
-        return f'z = {self.real} + {self.imaginary}*i'
+        return f'({self.real} + {self.imaginary}*i)'
     
     
 if __name__ == '__main__':
-    z1 = ComplexNumber(1, 3)
-    z2 = ComplexNumber(7, 2)
+    z1 = ComplexNumber(5, -6)
+    z2 = ComplexNumber(-3, 2)
+
     
-    print(z1)
-    print(z1 + z2)
-    print(z1 * z2)
+    print("печатаем комплексное число: ", z1)
+    print(f"сумма: {z1} + {z2} = ", z1 + z2)
+    print(f"разность: {z1} - {z2} = ", z1 - z2)
+    print(f"умножение: {z1} * {z2} = ", z1 * z2)
     
     z3 = ComplexNumber('5.2', 4)
-    print(z3)
+    print("Ввод float через str:", z3)
